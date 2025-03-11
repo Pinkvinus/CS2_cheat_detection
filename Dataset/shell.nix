44d8f5d526bc515277c126bc0277116b81ebc13b
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-03-11 12:04:44 +0100

    Added a functional less bloated version of the shell

--- Dataset/shell.nix
+++ Dataset/shell.nix
@@ -1,33 +1,32 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    python3
-    python3Packages.pip
-    python3Packages.setuptools
-    python3Packages.wheel
-    python3Packages.virtualenv
-    cmake
-    gcc
-    pkgs.stdenv.cc.cc.lib  # Ensure libstdc++.so.6 is available
+  name = "cs2_env";
+  buildInputs = [
+    pkgs.python3
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.numpy     # Demoparser2 dependency
+    pkgs.python3Packages.pandas    # Demoparser2 dependency
+    pkgs.python3Packages.polars    # Demoparser2 dependency
+    pkgs.python3Packages.pyarrow   # Demoparser2 dependency
+    pkgs.python3Packages.tqdm      # Demoparser2 dependency
+    pkgs.python3Packages.setuptools
+    pkgs.python3Packages.wheel
+    pkgs.python3Packages.virtualenv
   ];
 
   shellHook = ''
-    # Ensure the virtual environment exists
+    # Set up virtual environment if not already created
+    #     For some reason, dependencies for demoparser2 are not correctly set up with pip.
+    #     So the way it is done here is dependencies are installed in the mkShell.
+
     if [ ! -d ".venv" ]; then
       python -m venv .venv
-    fi
-    
-    # Activate the virtual environment
-    source .venv/bin/activate
-
-    # Upgrade pip and install demoparser2 if not already installed
-    if ! python -c "import demoparser2" &>/dev/null; then
+      source .venv/bin/activate
       pip install --upgrade pip
       pip install demoparser2
+    else
+      source .venv/bin/activate
     fi
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-
-    export PYTHONPATH="$(pwd)/.venv/lib/python3.10/site-packages:$PYTHONPATH"
   '';
 }
