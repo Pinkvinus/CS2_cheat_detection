4a2a7dfe5cd322cfa9372c83c6c9b0d2819fe1b5
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-03-25 13:38:56 +0100

    added handling of sensitive playerdata

--- Dataset/dem2datafile.py
+++ Dataset/dem2datafile.py
@@ -10,20 +10,31 @@ OUTPUT_PATH = "./Data"
 
 counter = 0
 
+
+
+
+
 for demo in os.listdir(INPUT_PATH):
 
     demu.parser = DemoParser(INPUT_PATH + "/" + demo)
     path = OUTPUT_PATH + "/" + str(counter)
 
     # loading the information
+    print("Parsing file")
     tick_df = demu.parser.parse_ticks(demu.ALL_FIELDS)
     events_list = demu.get_all_events()
 
-    # removing sensitive information
-    
+    print("Handling Ticks =========")
+    print("Replacing sensitive data")
+    tick_df = demu.replace_sensitive_data_df(tick_df)
+    print("Removing sensitive data")
+    tick_df = demu.remove_sensitive_data_df(tick_df)
 
-    tick_df = demu.replace_df_names(tick_df)
+    print("Handling Events ========")
+    events_list = demu.sensitive_data_events(events_list)
 
-    tick_df.to_csv(path_or_buf=path + ".csv")
+    print("Writting data to csv file")
+    tick_df.to_json(path_or_buf=path + "ticks.json")
 
-    demu.events_2_json(path + ".json")
+    print("Writting data to json file")
+    demu.event_list_2_json(events_list, path + ".json")
