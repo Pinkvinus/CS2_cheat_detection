5908b4529b761656134841278318b72ae4f5c682
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-04 10:02:48 +0200

    added support for skipping elements

--- Dataset/dem2datafile.py
+++ Dataset/dem2datafile.py
@@ -8,14 +8,15 @@ import os
 import sys
 import ast
 
-INPUT_PATH = "./Demos"
+INPUT_PATH = "./Demo_data/Demos"
 OUTPUT_PATH = "./Data"
 CHEATER_PATH = OUTPUT_PATH + "/with_cheater_present"
 NO_CHEATER_PATH = OUTPUT_PATH + "/no_cheater_present"
 
-SCRAPE_PATH = "cs_scrape_test.csv" # TODO: CHANGE BACK
+SCRAPE_PATH = "./Demo_data/cs_scrape_2025-04-03.csv"
 
-counter = 0
+counter_no_cheater = 0
+counter_cheater = 0
 
 scrape_df = pd.read_csv(SCRAPE_PATH)
 df = scrape_df.dropna(subset=["demo_file_name"])
@@ -24,6 +25,24 @@ df["cheater_names_str"] = df["cheater_names_str"].apply(ast.literal_eval)
 
 for index, row in df.iterrows():
 
+    # Looks up cheater information
+    cheaters = row["cheater_names_str"]
+    contains_cheaters = len(cheaters) > 0
+
+    cheater_filepath = CHEATER_PATH + "/" + str(counter_cheater)
+    no_cheater_filepath = NO_CHEATER_PATH + "/" + str(counter_no_cheater)
+
+    # check if this file has already been parsed
+    if contains_cheaters and os.path.isfile(cheater_filepath + ".json") and os.path.isfile(cheater_filepath + ".csv.gz"):
+        print(f"File '{counter_cheater}' already exists (with cheater)")
+        counter_cheater = counter_cheater + 1
+        continue
+    elif not contains_cheaters and os.path.isfile(no_cheater_filepath + ".json") and os.path.isfile(no_cheater_filepath + ".csv.gz"):
+        print(f"File '{counter_no_cheater}' already exists")
+        counter_no_cheater = counter_no_cheater + 1
+        continue
+
+    # Updating parser with correct information
     demu.parser = DemoParser(INPUT_PATH + "/" + row["demo_file_name"])
 
     # loading the informations
@@ -31,20 +50,19 @@ for index, row in df.iterrows():
     tick_df = demu.parser.parse_ticks(demu.ALL_FIELDS)
     events_list = demu.get_all_events()
 
-    # Add the cheater information to events_list
-    cheaters = row["cheater_names_str"]
-    
-    if len(cheaters) != 0:
+    if contains_cheaters:
+
         # Create a cheater dataframe
-        cheater_df = pd.DataFrame(columns=["cheater"])
+        cheater_df = pd.DataFrame(columns=["name"])
         
         for c in cheaters:
-            new_row = pd.DataFrame({"cheater":[c]})
+            new_row = pd.DataFrame({"name":[c]})
             cheater_df = pd.concat([cheater_df, new_row], ignore_index=True)
         
         # add cheater dataframe to events_list
         events_list.append(("cheaters", cheater_df))
 
+    # Add other info from csstats to events
     map_df = pd.DataFrame({"map":[row["map"]], "server":[row["server"]],"avg_rank":[row["avg_rank"]],"match_making_type":[row["type"]]})
 
     events_list.append(("CSstats_info", map_df))
@@ -60,15 +78,17 @@ for index, row in df.iterrows():
     print("Handling Events ========")
     events_list = demu.sensitive_data_events(events_list)
 
-    path = NO_CHEATER_PATH + "/" + str(counter)
+    path = ""
 
     if len(cheaters) != 0:
-        path = CHEATER_PATH + "/" + str(counter)
+        path = CHEATER_PATH + "/" + str(counter_cheater)
+        counter_cheater = counter_cheater + 1
+    else:
+        path = NO_CHEATER_PATH + "/" + str(counter_no_cheater)
+        counter_no_cheater = counter_no_cheater + 1
 
     print("Writting data to csv file")
     tick_df.to_csv(path_or_buf=path + ".csv.gz", compression='gzip')
 
     print("Writting data to json file")
     demu.event_list_2_json(events_list, path + ".json")
-
-    counter = counter + 1
