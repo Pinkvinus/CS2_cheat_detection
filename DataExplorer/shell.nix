1774e6f389074c23b4cc230cf50ca981d62f3fb4
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2025-04-10 12:29:59 +0200

    added shell hint

--- DataExplorer/shell.nix
+++ DataExplorer/shell.nix
@@ -1,5 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
+# In order to run jupyternb in vscode, run 'nix-shell' in a 
+# terminal, and open vscode in that same terminal using 'code .'
+
 pkgs.mkShell {
   name = "cs2_env";
   nativeBuildInputs = with pkgs; [
