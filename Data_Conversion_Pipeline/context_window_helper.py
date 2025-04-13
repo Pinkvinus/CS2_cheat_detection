ee5bff8c07533766409535b499764f1fc04dfeeb
gelu <gelu@itu.dk>
2025-04-13 13:47:53 +0300

    small progress on pitch yaw

--- Data_Conversion_Pipeline/context_window_helper.py
+++ Data_Conversion_Pipeline/context_window_helper.py
@@ -7,6 +7,10 @@ class MatchDataProcessor:
     def get_player_kills(self, events, player_name, player_death_idx):
         player_deaths = events[player_death_idx][1]
         return player_deaths[player_deaths["attacker_name"] == player_name]
+    
+    def get_victim_names(self, events, player_name, player_death_idx):
+        victims = events[player_death_idx][1]
+        return player_deaths[player_deaths["attacker_name"] == player_name]
 
     def get_context_window_ticks(self, player_deaths, ticks_before_kill, tick_after_kill):
         start_ticks = []
@@ -37,6 +41,35 @@ class MatchDataProcessor:
                 delta_values.append(self.yaw_delta(values[i-1], values[i]))
 
         return delta_values
+    
+    def get_pitch_yaw_head_deltas(self, key, start_tick, end_tick, attacker, victim):
+        delta_values = []
+        context_window_size = end_tick - start_tick
+        values = self.match_ticks[(self.match_ticks["name"] == attacker) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)][key].tolist()
+        attacker_X_values = self.match_ticks[(self.match_ticks["name"] == attacker) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["X"].tolist()
+        attacker_Y_values = self.match_ticks[(self.match_ticks["name"] == attacker) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["Y"].tolist()
+        attacker_Z_values = self.match_ticks[(self.match_ticks["name"] == attacker) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["Z"].tolist()
+        victim_X_values = self.match_ticks[(self.match_ticks["name"] == victim) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["X"].tolist()
+        victom_Y_values = self.match_ticks[(self.match_ticks["name"] == victim) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["Y"].tolist()
+        victom_Z_values = self.match_ticks[(self.match_ticks["name"] == victim) & 
+                                 (self.match_ticks["tick"] <= end_tick) & 
+                                 (self.match_ticks["tick"] > start_tick)]["Z"].tolist()
+        if key == "pitch":
+            ...
+        elif key == "yaw":
+            ...
     
     def pitch_delta(self, a1, a2):
         """Returns the shortest difference between two angles in degrees (-180 to 180)"""
